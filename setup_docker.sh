724234e12d91b16cb531e1289a4defddb8d2dee3
hardik <hardikparwa@github.com>
2023-05-30 11:01:46 -0400

    made setup script to copy over model files

---
+++ setup_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -r /home/hsr/catkin_ws/src/hsr_demo/worlds/models/hsr_ground_plane/ /opt/ros/noetic/share/tmc_gazebo_worlds/models/hsr_ground_plane
+cp -r /home/hsr/catkin_ws/src/hsr_demo/worlds/models/hsr_corridor_environment/ /opt/ros/noetic/share/tmc_gazebo_worlds/models/hsr_corridor_environment
+cp -r /home/hsr/catkin_ws/src/hsr_demo/worlds/models/hsr_wall_environment/ /opt/ros/noetic/share/tmc_gazebo_worlds/models/hsr_wall_environment/
