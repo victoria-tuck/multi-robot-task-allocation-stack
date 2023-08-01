e97b65237cc96e1706e2e223c0b9177c8b4619d5
hardik <parwanahardik@gmail.com>
2023-08-01 13:02:09 -0400

    added ignition model location to gazebo_model_path

--- colcon_ws/src/aws-robomaker-hospital-world/launch/view_hospital.launch.py
+++ colcon_ws/src/aws-robomaker-hospital-world/launch/view_hospital.launch.py
@@ -4,10 +4,12 @@ import sys
 import launch
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import SetEnvironmentVariable
 
 
 def generate_launch_description():
     ld = launch.LaunchDescription([
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value='$GAZEBO_MODEL_PATH:/home/colcon_ws/src/aws-robomaker-hospital-world/ignition_models'),
         launch.actions.IncludeLaunchDescription(
             launch.launch_description_sources.PythonLaunchDescriptionSource(
                 [get_package_share_directory(
