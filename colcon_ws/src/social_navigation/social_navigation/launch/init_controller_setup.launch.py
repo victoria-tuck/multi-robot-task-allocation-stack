0a1cc55c0dd4c67febb359f33824e29663204f66
Victoria Tuck <victoria.tuck@toyota.com>
2024-07-10 11:06:41 -0400

    Adding setup via file into init_controller

--- colcon_ws/src/social_navigation/social_navigation/launch/init_controller_setup.launch.py
+++ colcon_ws/src/social_navigation/social_navigation/launch/init_controller_setup.launch.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import argparse
+import json
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
@@ -9,7 +12,11 @@ from launch.substitutions import LaunchConfiguration, PythonExpression
 
 def generate_launch_description():
     
-    social_navigation_dir = get_package_share_directory('social_navigation')    
+    social_navigation_dir = get_package_share_directory('social_navigation')  
+
+    arguments = parse_arguments(sys.argv)
+    scenario_file = get_scenario_file_from_arguments(arguments)
+    robot_num = get_robot_count(scenario_file)
     
     use_sim_time = LaunchConfiguration('use_sim_time')
     declare_use_sim_time_cmd = DeclareLaunchArgument(
@@ -45,7 +52,7 @@ def generate_launch_description():
             'use_sim_time': use_sim_time,
             'num_humans': num_humans
             }],
-        arguments=["-num_robots", "4"]
+        arguments=["-num_robots", str(robot_num)]
     )   
 
     human_closest_obstacle_sector = Node(
@@ -67,3 +74,31 @@ def generate_launch_description():
     ld.add_action(robot_closest_obstacle_sector)
     ld.add_action(human_closest_obstacle_sector)
     return ld
+
+
+def get_robot_count(file):
+    with open(file, 'r') as f:
+        scenario_setup = json.load(f)
+    robot_num = len(scenario_setup["agents"].values())
+    return robot_num
+
+
+def get_scenario_file_from_arguments(argv):
+    print(f"Arguments: {argv}")
+    parser = argparse.ArgumentParser(
+        description='Start robot goal setters'
+    )
+    parser.add_argument('-input_file', type=str, help='Scenario file')
+    args = parser.parse_args(argv)
+    return args.input_file
+
+
+def parse_arguments(argv):
+    args = []
+    for arg in argv:
+        if ":=" in arg:
+            parsed_arg = arg.split(':')
+            print(parsed_arg)
+            args.append(f"-{parsed_arg[0]}")
+            args.append(parsed_arg[1][1:])
+    return args
