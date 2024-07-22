7e3a83dcf4d6827027490eb43bea91e55cc655d7
Victoria Tuck <victoria.tuck@toyota.com>
2024-07-22 15:57:49 -0400

    Added more params into travel_time_collector params file

--- colcon_ws/src/social_navigation/social_navigation_py/social_navigation_py/travel_time_collector.py
+++ colcon_ws/src/social_navigation/social_navigation_py/social_navigation_py/travel_time_collector.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.clock import Clock
 from rclpy.executors import MultiThreadedExecutor
-from nav2_simple_commander.robot_navigator import BasicNavigator
 import csv
 import json
 
@@ -24,52 +22,37 @@ class TravelTimeCollector(Node):
         else:
             prefix = ""
 
-        self.declare_parameter('locations_file', rclpy.Parameter.Type.STRING)
+        self.declare_parameter('time_collection_params', rclpy.Parameter.Type.STRING)
         self.declare_parameter('save_file', rclpy.Parameter.Type.STRING)
-        locations_file = self.get_parameter('locations_file').value
+        time_collection_params = self.get_parameter('time_collection_params').value
 
-        with open(locations_file, 'r') as f:
-            map_locations = json.load(f)['map_locations']
-        self.locs = [(loc[0], loc[1]) for loc in map_locations]
+        with open(time_collection_params, 'r') as f:
+            params = json.load(f)
+        self.max_iterations, self.save_mode = params["iterations"], params["save_mode"]
+        self.locs = [(loc[0], loc[1]) for loc in params["map_locations"]]
         self.save_file = self.get_parameter('save_file').value
 
-        self.subscriber = self.create_subscription( PoseArray, prefix + '/goal_sequence', self.goal_sequence_callback, 10 )
-        # self.subscriber = self.create_subscription( PoseArray, '/goal_sequence', self.goal_sequence_callback, 10 )
-
         self.publisher_ = self.create_publisher(PoseStamped, prefix + '/goal_location', 1)
-        # self.publisher_ = self.create_publisher(PoseStamped, '/goal_location', 1)
+
         self.timer_period = 1.0
         self.timer = self.create_timer(self.timer_period, self.publish_goal)
-        self.name = name
         self.location_listener = self.create_subscription(PoseStamped, prefix + '/robot_location', self.listen_location_callback, 1)
-        # self.location_listener = self.create_subscription(PoseStamped, '/robot_location', self.listen_location_callback, 1)
 
-        # self.navigator = BasicNavigator()
-        # self.delivery_locations = [(0, 2.2), (4.25, -27.5), (-7.75, -21.7), (7.85, -21.8), (7.9, -7.5), (-7.75, -7.5)]
-        # self.locs = [(0, 2.2), (4.25, -27.5), (-7.75, -21.7), (7.85, -21.8), (7.9, -7.5), (-7.75, -7.5)] # Old locations in weird order
-        # self.locs = [(0, 2.2), (7.9, -7.5), (7.85, -21.8), (4.25, -27.5), (-7.75, -21.7), (-7.75, -7.5)]
-        # self.locs = [(0, 2.2), (4.25, -27.5), (-7.75, -21.7)]
+        location_indices = list(range(len(self.locs)))
+        assert len(location_indices) > 1
+        self.travel_times = {key1: {key2: [] for key2 in location_indices if key1 != key2} for key1 in location_indices}
+
         self.starting_idx = 0
         self.ending_idx = 1
         self.goal_idx = 1
         self.iteration = 1
-        self.max_iterations = 1
+        self.loc_idx = 0
+
         self.traveling_to_start = False
         self.finished = False
-        location_indices = list(range(len(self.locs)))
-        self.travel_times = {key1: {key2: [] for key2 in location_indices if key1 != key2} for key1 in location_indices}
-        self.arrived_at_start = False
-        self.pull_travel_time = False
         self.start_time = None
-        self.loc_idx = 0
         self.data_saved = False
-
         self.goal_reached = True
-
-    def goal_sequence_callback(self, msg):
-        self.locs = [(pose.position.x, pose.position.y) for pose in msg.poses]
-        self.loc_idx = 0
-        print(f"{self.name} updated its goals: {self.locs}")
 
     def publish_goal(self):
         if self.goal_idx < len(self.locs) and not self.finished:
@@ -78,7 +61,6 @@ class TravelTimeCollector(Node):
                 msg = PoseStamped()
                 msg.header.frame_id = "map"
                 msg.header.stamp = self.get_clock().now().to_msg()
-                # msg.header.stamp = self.navigator.get_clock().now().to_msg()
                 msg.pose.position.x = float(goal[0])
                 msg.pose.position.y = float(goal[1])
                 msg.pose.position.z = 0.01
@@ -98,12 +80,19 @@ class TravelTimeCollector(Node):
                 write = csv.writer(f)
                 for key1, sub_dict in self.travel_times.items():
                     for key2, travel_time_list in sub_dict.items():
-                        write.writerow([key1] + [key2] + travel_time_list)
+                        header = [key1, key2]
+                        if self.save_mode == "MAX_CEIL":
+                            write.writerow(header + [math.ceil(max(travel_time_list))])
+                        elif self.save_mode == "MAX":
+                            write.writerow(header + [max(travel_time_list)])
+                        elif self.save_mode == "AVG":
+                            write.writerow(header + [sum(travel_time_list)/len(travel_time_list)])
+                        elif self.save_mode == "ALL":
+                            write.writerow([key1, key2] + travel_time_list)
             self.data_saved = True
 
     def listen_location_callback(self, msg):
         cur_loc = (msg.pose.position.x, msg.pose.position.y)
-        # print(f"Received {self.name}'s current location: {cur_loc}")
         if self.goal_idx < len(self.locs):
             if dist(self.locs[self.goal_idx], cur_loc) < DIST_THRES:
                 if self.start_time is not None:
@@ -140,14 +129,11 @@ class TravelTimeCollector(Node):
                         self.goal_idx = self.ending_idx
                 else:
                     self.goal_idx = self.starting_idx
-                # if self.loc_idx + 1 > len(self.locs):
-                #     self.start_time = None
-                # self.loc_idx = min(self.loc_idx + 1, len(self.locs) - 1)
                 self.goal_reached = True
 
 def main(args=None):
     rclpy.init(args=args)
-    goal_publisher1 = GoalSetter(name="robot1")
+    goal_publisher1 = TravelTimeCollector(name="robot1")
     executor = MultiThreadedExecutor()
     executor.add_node(goal_publisher1)
     executor.spin()
