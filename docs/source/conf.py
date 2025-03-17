55beede44d66badb57fe61c77ccaa5fe955e8bb1
Victoria Tuck <victoria_tuck@berkeley.edu>
2025-03-17 16:34:36 -0700

    Trying out doc conf configurations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ author = 'Victoria Tuck, Hardik Parwana, Pei-Wei Chen, Georgios Fainekos, Bardh 
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
+    # 'myst_parser',
 ]
 
 templates_path = ['_templates']
@@ -25,5 +25,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme" # 'alabaster'
 html_static_path = ['_static']
